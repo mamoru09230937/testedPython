440028482b81651d788ed6acd26f864dd05940e8
marumo.kudo <mamoru09230937@gmail.com>
2019-11-17 12:50:44 +0900

    オブジェクトもどきの生成

--- python/sampleEmptyChart.py
+++ python/sampleEmptyChart.py
@@ -1,12 +1,26 @@
 from openpyxl import Workbook
-from openpyxl.chart import BarChart
+from openpyxl.chart import BarChart , Reference
+from openpyxl.chart.layout import Layout, ManualLayout
 
 wb = Workbook()
 ws = wb.active
 
+data  =[[]]
 def createEmptyChart(ws) :
+    
+    for row in data:
+        ws.append(row)
+    values = Reference(ws, min_col = 1, max_col=1,min_row= 1, max_row = 1)
     chart = BarChart()
+    chart.add_data(values)
+    chart.x_axis.allow_none = True
+    chart.y_axis.allow_none = True
+    chart.y_axis.majorGridlines = None
+    chart.legend = None
+    chart.x_axis.delete = True
+    chart.y_axis.delete = True
     chart.title ='Test'
+
     ws.add_chart(chart , 'A1')
 
 createEmptyChart(ws)
